ffee0dd23c281a47a5bb5cce6554eb2c04dbfdf5
Tori <torigcook@gmail.com>
2025-08-17 11:55:40 -0700

    make sanity use env vars

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,10 @@
+
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
-
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-08-13',
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  token: process.env.SANITY_API_TOKEN, // Only needed for mutations
 })
